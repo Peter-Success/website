5c16ec7bed2909f8d81b85097137ad6bfab856d7
lijianxin <lijianxin_max@sina.com>
2023-12-30 15:06:06 +0800

    微整形完成

--- pages/fwxm/wzx.vue
+++ pages/fwxm/wzx.vue
@@ -20,9 +20,9 @@
       <!--      咨询部分-->
       <div class="consult">
         <div class="consult-topBox">
-          <img class="consult-topBox-img" src="~/assets/images/wzx/wzx-zxImg.png" alt="">
+          <img class="consult-topBox-img" src="~/assets/images/wzx/wzx-zxImg.jpg" alt="">
           <div class="consult-topBox-textBox">
-            <div class="title">紧肤溶脂提升一次搞定</div>
+            <div class="title">冻龄抗衰免动刀，轻轻松松焕然一新</div>
             <p>
               如果只是希望可以进行面部的微调，你还是你自己，只是让五官、整体面部比例更加精致化，现在有很多微整的方式，透过注射、将生物材料或人工合成生物兼容性材料注射入真皮层或皮下等方式，可以在不破坏面部结构的前提之下，达到你想要的效果，让面部年轻化、外表更有魅力，因此微整形是当下很多爱美朋友在做医疗美容时的首选。</p>
             <p>哪些人适合微整型？</p>
@@ -35,7 +35,7 @@
 
         <img @click="gotoLxzx" class="consult-btn" src="~/assets/images/wzx/wzx-zxBtn.png" alt="">
 
-        <img class="consult-bottomBox" src="~/assets/images/wzx/wzx-zxBg.png" alt="">
+        <img class="consult-bottomBox" src="~/assets/images/wzx/wzx-zxBg.jpg" alt="">
       </div>
 
       <!--      详解部分-->
@@ -49,11 +49,11 @@
             <p class="text">肉毒素可暂时使肌肉放松、缩小，进而达到修容效果，常用来瘦脸、除皱、提升面颈部轮廓、瘦腿、瘦肩和除腋臭等。肉毒素注射是目前对眉间纹、鱼尾纹和抬头纹最有效预防方式，一般情况下，在注射
               3-7 天内产生效果。</p>
           </div>
-          <img class="headImg" src="~/assets/images/wzx/wzx-xj1.png" alt="">
+          <img class="headImg" src="~/assets/images/wzx/wzx-xj1.jpg" alt="">
         </li>
 
         <li class="detail-item">
-          <img class="headImg" src="~/assets/images/wzx/wzx-xj2.png" alt="">
+          <img class="headImg" src="~/assets/images/wzx/wzx-xj2.jpg" alt="">
 
           <div class="detail-item-textBox">
             <div class="title">
@@ -73,11 +73,11 @@
             </div>
             <p class="text">胶原蛋白植入剂主要成份为第一型胶原蛋白，它是人体分布最广的胶原蛋白，也是皮肤含量最高之胶原蛋白，由三条多胜肽链相互缠绕而成，其具有提供组织韧性及弹性，同时有止血、受损组织修补等功能特性。胶原蛋白针可改善脸部及颈部的肌肤松弛、面容轮廓线微调和轻微凹陷填补等等。由于其材料物理美白遮瑕的特性，其最受欢迎的用途就是改善黑眼圈。</p>
           </div>
-          <img class="headImg" src="~/assets/images/wzx/wzx-xj3.png" alt="">
+          <img class="headImg" src="~/assets/images/wzx/wzx-xj3.jpg" alt="">
         </li>
 
         <li class="detail-item">
-          <img class="headImg" src="~/assets/images/wzx/wzx-xj4.png" alt="">
+          <img class="headImg" src="~/assets/images/wzx/wzx-xj4.jpg" alt="">
 
           <div class="detail-item-textBox">
             <div class="title">
@@ -99,7 +99,7 @@
               微型晶球仍会持续作用，诱发自体新生胶原蛋白，并逐渐取代也会被人体所吸收的 PCL 微型晶球。少女针由于其材料物理支撑力强的特性，为打造精致五官轮廓首选，并可实现轮廓固定，达到全脸拉提的效果。</p>
           </div>
 
-          <img class="headImg" src="~/assets/images/wzx/wzx-xj5.png" alt="">
+          <img class="headImg" src="~/assets/images/wzx/wzx-xj5.jpg" alt="">
 
         </li>
       </ul>
@@ -125,7 +125,7 @@
           <p>10、加强保湿，避免皮肤因缺水老化产生细纹皱纹。</p>
           <p>◆有任何疑虑请安排回诊咨询。</p>
         </div>
-        <img class="headImg" src="~/assets/images/wzx/wzx-zyImg.png" alt="">
+        <img class="headImg" src="~/assets/images/wzx/wzx-zyImg.jpg" alt="">
       </div>
     </div>
 
@@ -149,9 +149,9 @@
       <!--      咨询部分-->
       <div class="consult">
         <div class="consult-topBox">
-          <img class="consult-topBox-img" src="~/assets/images/wzx/wzx-zxImg.png" alt="">
+          <img class="consult-topBox-img" src="~/assets/images/wzx/wzx-zxImg.jpg" alt="">
           <div class="consult-topBox-textBox">
-            <div class="title">紧肤溶脂提升一次搞定</div>
+            <div class="title">冻龄抗衰免动刀，轻轻松松焕然一新</div>
             <p>
               如果只是希望可以进行面部的微调，你还是你自己，只是让五官、整体面部比例更加精致化，现在有很多微整的方式，透过注射、将生物材料或人工合成生物兼容性材料注射入真皮层或皮下等方式，可以在不破坏面部结构的前提之下，达到你想要的效果，让面部年轻化、外表更有魅力，因此微整形是当下很多爱美朋友在做医疗美容时的首选。</p>
             <p>哪些人适合微整型？</p>
@@ -162,7 +162,7 @@
           </div>
         </div>
 
-        <img class="consult-bottomBox" src="~/assets/images/wzx/wzx-zxBg.png" alt="">
+        <img class="consult-bottomBox" src="~/assets/images/wzx/wzx-zxBg.jpg" alt="">
       </div>
 
       <!--      详解部分-->
@@ -173,7 +173,7 @@
               <div>肉毒素</div>
             </div>
           </div>
-          <img class="headImg" src="~/assets/images/wzx/wzx-xj1.png" alt="">
+          <img class="headImg" src="~/assets/images/wzx/wzx-xj1.jpg" alt="">
           <p class="text">肉毒素可暂时使肌肉放松、缩小，进而达到修容效果，常用来瘦脸、除皱、提升面颈部轮廓、瘦腿、瘦肩和除腋臭等。肉毒素注射是目前对眉间纹、鱼尾纹和抬头纹最有效预防方式，一般情况下，在注射
             3-7 天内产生效果。</p>
         </li>
@@ -184,7 +184,7 @@
               <div>玻尿酸</div>
             </div>
           </div>
-          <img class="headImg" src="~/assets/images/wzx/wzx-xj2.png" alt="">
+          <img class="headImg" src="~/assets/images/wzx/wzx-xj2.jpg" alt="">
           <p class="text"> 玻尿酸又称为透明质酸（Hyaluronic Acid,
             HA），是人体自然形成的天然物质，广泛存在于人体细胞外基质、玻璃体体液和软骨中，本质是一种天然多糖，因此对人体无毒无害。玻尿酸作为保湿保养品可以替肌肤表层进行补水；而作为填充剂则可以从深层将皱纹所形成的肌肤凹陷撑起来，可有效改善静态皱纹如泪沟、法令纹、木偶纹，以及修饰五官轮廓和达到紧致拉提等作用。</p>
         </li>
@@ -195,7 +195,7 @@
               <div>胶原蛋白</div>
             </div>
           </div>
-          <img class="headImg" src="~/assets/images/wzx/wzx-xj3.png" alt="">
+          <img class="headImg" src="~/assets/images/wzx/wzx-xj3.jpg" alt="">
           <p class="text">胶原蛋白植入剂主要成份为第一型胶原蛋白，它是人体分布最广的胶原蛋白，也是皮肤含量最高之胶原蛋白，由三条多胜肽链相互缠绕而成，其具有提供组织韧性及弹性，同时有止血、受损组织修补等功能特性。胶原蛋白针可改善脸部及颈部的肌肤松弛、面容轮廓线微调和轻微凹陷填补等等。由于其材料物理美白遮瑕的特性，其最受欢迎的用途就是改善黑眼圈。</p>
         </li>
 
@@ -205,7 +205,7 @@
               <div>童颜针</div>
             </div>
           </div>
-          <img class="headImg" src="~/assets/images/wzx/wzx-xj4.png" alt="">
+          <img class="headImg" src="~/assets/images/wzx/wzx-xj4.jpg" alt="">
           <p class="text">童颜针材料为聚左旋乳酸（PLLA），是一种胶原蛋白的刺激物，注射进入人体后，可以刺激纤维母细胞，促使胶原蛋白新生，进而让皮肤变得有弹力、紧实。此外，也可以改善肌肤流失胶原蛋白所产生的凹陷与皱纹，达到填充的效果。需要特别留意的是，童颜针主要采渐进式的方式来促进胶原蛋白的再生，效果为慢慢自然的呈现，因此需要疗程治疗，注射后通常需给予 1 个月左右的时间才会看见效果，非常适合想低调变美的人群。值得一提的是，注射童颜针后需遵守 555（每天 5 次、每次 5 分钟、连续 5 天）的按摩护理法则，让PLLA注射物均匀分散至皮肤，达到最自然的胶原蛋白增生效果。</p>
         </li>
 
@@ -215,7 +215,7 @@
               <div>少女针</div>
             </div>
           </div>
-          <img class="headImg" src="~/assets/images/wzx/wzx-xj5.png" alt="">
+          <img class="headImg" src="~/assets/images/wzx/wzx-xj5.jpg" alt="">
           <p class="text"> 伊妍仕 Ellansé，即俗称的少女针。由 30%的聚己内酯 PCL 微型晶球及 70%的 CMC 凝疑胶载体所组成。CMC
             疑胶体注射至皮肤下后，可以支撑凹陷处，提供即时填补、皱纹修复的效果；PCL 微型晶球则会刺激胶原蛋白新生，因此即使在 CMC 凝胶体在人体内自然分解吸收（且不残留）后，PCL
             微型晶球仍会持续作用，诱发自体新生胶原蛋白，并逐渐取代也会被人体所吸收的 PCL 微型晶球。少女针由于其材料物理支撑力强的特性，为打造精致五官轮廓首选，并可实现轮廓固定，达到全脸拉提的效果。</p>
@@ -230,7 +230,7 @@
       <!--      注意事项-->
       <div class="attention">
         <div class="attention-title">术后注意事项：</div>
-        <img class="headImg" src="~/assets/images/wzx/wzx-zyImg.png" alt="">
+        <img class="headImg" src="~/assets/images/wzx/wzx-zyImg.jpg" alt="">
         <div class="attention-textBox">
           <p>1、注射后，因个人体质，皮肤可能会出现发红、肿胀、疼痛、瘀青等情形均属正常状况。在注射几天后，这些反应会自动缓解。</p>
           <p>2、注射后前3 天冰敷可帮助红肿减退，肿胀持续约1周左右消失。</p>
